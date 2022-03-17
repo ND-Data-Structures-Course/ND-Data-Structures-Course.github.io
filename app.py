d40a893800a096c99a110eb48a03a734e654e36f
varora24 <varora@nd.edu>
2022-03-17 13:48:18 -0400

    updated css and flask for a whole bunch of stuff

--- app.py
+++ app.py
@@ -6,52 +6,33 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-URL = "https://jsonkeeper.com/b/E5YJ"
+LEARNPAGE_URL = "https://jsonkeeper.com/b/SM6B"
+PRACTICE_URL = ""
 @app.route('/', methods=['POST','GET'])
 def index():
 	return render_template("index.html") #will need to edit later
-	''' Sample Code
-    response = requests.get(URL)
-    data = response.json()
-    data = bubble_sort_func(data, 'Maxplayers')
 
-    if request.method == 'GET': # GET request is sent when html wants some information from the python script
-        return render_template('index.html', numgames = 0)
-    
-    if request.method == 'POST': # POST request is sent when the html is sending sdata to the python code for processing
-        numplayers=request.form.get('numplayers')
-        genre = request.form.get('genre')
-        mode = request.form.get('mode')
-
-        dataout = findmatches(data, numplayers, genre, mode)
-        length = len(dataout)
- 
-    return render_template("index.html", numgames = length, games = dataout,numelements = True) '''
-
-def check_valid(data):
-	pass #remove this line when you want to actually run the function
-	# if information is missing, add default values here
-	'''default = {"Title": "Error XXX","Para1":"","Para2":""} #based on the layout we go for
-    for attribute in defaults:
-        if attribute not in data:
-            data[attribute] = defaults[attribute]'''
-
-@app.route('/watchvideo', methods=['POST','GET'])
-def watchvideo():
-    if request.method == 'POST':
-        #temp = request.form.get('WATCH')
-        return redirect('https://youtube.com/') # TOD0 - replace with link to the required Youtube playlist
-    else:
-        return render_template("index.html")
-
+def getData(chap_id):
+    # scan through all data and get the required information for the relevant chapter based on ID.
+    return [] #should return list of dictionariies of images and paragraphs
 
 @app.route('/learn', methods=['POST','GET']) #for learn page
 def learn_page():
-	response = requests.get(URL)
-	data = response.json()
-	print(data)
-	# add return statement here to render template
-	return render_template("learn.html",chapters=data) 
+    response = requests.get(LEARNPAGE_URL)
+    data = response.json()
+    if request.method == 'GET':
+        return render_template("learn.html",chapters=data)
+    if request.method == 'POST':
+        chapter_id=request.form.get('id')
+        currentchapterdata = getData(chapter_id)
+        #make it go to new function generating the new html page
+        return render_template("learn.html",chapters=data) 
+
+
+@app.route('/studyguide', methods=['POST','GET'])
+def data_page():
+    infoset = [] #make a list of all paras and images as needed
+    return render_template("studyguide.html",)
 
 
 @app.route('/contact', methods=['POST','GET']) #for contact page
