f77d6e978cecb1f4312c524e37b8c4cfc3ebe741
varora24 <varora@nd.edu>
2022-03-17 16:07:11 -0400

    updated code and look of website. Now works for individual webpages

--- app.py
+++ app.py
@@ -8,13 +8,10 @@ app = Flask(__name__)
 
 LEARNPAGE_URL = "https://jsonkeeper.com/b/SM6B"
 PRACTICE_URL = ""
+DATA_URL=""
 @app.route('/', methods=['POST','GET'])
 def index():
 	return render_template("index.html") #will need to edit later
-
-def getData(chap_id):
-    # scan through all data and get the required information for the relevant chapter based on ID.
-    return [] #should return list of dictionariies of images and paragraphs
 
 @app.route('/learn', methods=['POST','GET']) #for learn page
 def learn_page():
@@ -24,15 +21,12 @@ def learn_page():
         return render_template("learn.html",chapters=data)
     if request.method == 'POST':
         chapter_id=request.form.get('id')
-        currentchapterdata = getData(chapter_id)
+        chaptertitle = data[int(chapter_id[0])-1].get('Chapters')[int(chapter_id[2])-1].get('Title')
+        print(chaptertitle)
+        # TODO: get information for topic from database and store the data into a list and pass it into the website
+        return render_template("studyguide.html",title=chaptertitle,information=[])
         #make it go to new function generating the new html page
-        return render_template("learn.html",chapters=data) 
-
-
-@app.route('/studyguide', methods=['POST','GET'])
-def data_page():
-    infoset = [] #make a list of all paras and images as needed
-    return render_template("studyguide.html",)
+        #return render_template("learn.html",chapters=data) 
 
 
 @app.route('/contact', methods=['POST','GET']) #for contact page
