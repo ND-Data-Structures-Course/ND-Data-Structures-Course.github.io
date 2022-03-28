d7201ac7cebb86202afdb23c3a594a884e8d25af
varora24 <varora@nd.edu>
2022-03-27 22:31:06 -0400

    added compatibility for multiple elements in blog

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Database URL's - Course Specific
 LEARNPAGE_URL = "https://raw.githubusercontent.com/ND-Data-Structures-Course/ND-Data-Structures-Course.github.io/master/learn.json"
 PRACTICE_URL = "https://raw.githubusercontent.com/ND-Data-Structures-Course/ND-Data-Structures-Course.github.io/master/practice.json"
-DATA_URL="https://raw.githubusercontent.com/ND-Data-Structures-Course/ND-Data-Structures-Course.github.io/master/chapterdata.json"
+DATA_URL="https://jsonkeeper.com/b/2QJ3" #"https://raw.githubusercontent.com/ND-Data-Structures-Course/ND-Data-Structures-Course.github.io/master/chapterdata.json"
 # End of Database URL's 
 
 @app.route('/', methods=['POST','GET'])
@@ -25,10 +25,13 @@ def learn_page():
         return render_template("learn.html",chapters=data)
     if request.method == 'POST':
         chapter_id=request.form.get('id')
+        chapterData = []
         chaptertitle = data[int(chapter_id[0])-1].get('Chapters')[int(chapter_id[2])-1].get('Title')
         response = requests.get(DATA_URL)
         allData = response.json()
-        chapterData = allData[int(chapter_id[0])-1].get('content')
+        for info in allData:
+            if info.get('unique_id') == chapter_id:
+                chapterData = info.get('content')
         print(chapterData)
         return render_template("studyguide.html",title=chaptertitle,information=chapterData)
 
